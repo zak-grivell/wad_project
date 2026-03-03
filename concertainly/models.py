40ab87199ca0d456cf339571b55753c866dee391
zak grivell <zak@grivell.uk>
2026-03-03 19:02:21 +0000

    added models

--- concertainly/models.py
+++ concertainly/models.py
@@ -1,3 +1,54 @@
+import uuid
+from django.db.models.deletion import CASCADE
+from django.core.validators import MinValueValidator, MaxValueValidator
 from django.db import models
 
-# Create your models here.
+
+class Artist(models.Model):
+    id = models.UUIDField(default=uuid.uuid4, editable=False, unique=True)
+    name = models.CharField(max_length=128)
+
+
+class User(models.Model):
+    id = models.UUIDField(default=uuid.uuid4, editable=False, unique=True)
+    name = models.CharField(max_length=128)
+    password = models.CharField(max_length=64)
+
+class Tour(models.Model):
+    id = models.UUIDField(default=uuid.uuid4, editable=False, unique=True)
+    name = models.CharField(max_length=128)
+    artist = models.ForeignKey(Artist, on_delete=CASCADE)
+
+
+class Song(models.Model):
+    id = models.UUIDField(default=uuid.uuid4, editable=False, unique=True)
+    name = models.CharField(max_length=128)
+    artist = models.ForeignKey(Artist, on_delete=CASCADE)
+
+
+class Review(models.Model):
+    id = models.UUIDField(default=uuid.uuid4, editable=False, unique=True)
+    title = models.CharField(max_length=128)
+    thoughts = models.CharField(max_length=1024)
+    img_path = models.CharField(max_length=128)
+
+    city = models.CharField(max_length=128)
+    venue = models.CharField(max_length=128)
+    date = models.DateField()
+
+    rating = models.IntegerField(
+        validators=[
+            MinValueValidator(0),
+            MaxValueValidator(5),
+        ]
+    )
+
+    user = models.ForeignKey(User, on_delete=CASCADE)
+    tour = models.ForeignKey(Tour, on_delete=CASCADE)
+
+    set_list = models.ManyToManyField(Song)
+
+
+class Genre:
+    id = models.UUIDField(default=uuid.uuid4, editable=False, unique=True)
+    name = models.CharField(max_length=128)
