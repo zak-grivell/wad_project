5a44fe80f98a615ade3454f87c5e33d0a4f72f87
zak grivell <zak@grivell.uk>
2026-03-03 17:17:40 +0000

    add the conertainly app

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # wad_project
+
+- get python 3.13 
+- then use pip install –r requirements.txt
+- run like normal django
--- concertainly/views.py
+++ concertainly/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+
+
+def index(request):
+    return HttpResponse("Concertainly says hey there partner!")
+
 
 # Create your views here.
