205b9a1970ee2e8ded362984a07bbb4a90a83bf5
zak grivell <zak@grivell.uk>
2026-03-03 17:27:24 +0000

    add templates

--- concertainly/views.py
+++ concertainly/views.py
@@ -1,8 +1,10 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
 
 def index(request):
-    return HttpResponse("Concertainly says hey there partner!")
+    context_dict = {"boldmessage": "Crunchy, creamy, cookie, candy, cupcake!"}
+    return render(request, "index.html", context=context_dict)
 
 
 # Create your views here.
