46f9d3f999ee97c7638608a2a92b556dea5e9854
zak grivell <zak@grivell.uk>
2026-03-03 19:02:21 +0000

    added models

--- devenv.nix
+++ devenv.nix
@@ -1,15 +1,9 @@
 { pkgs, ... }:
 
 {
-  languages.python = {
-    enable = true;
-
-    lsp.package = pkgs.ty;
-
-    uv.enable = true;
-  };
-
   packages = with pkgs; [
     ruff
+    uv
+    ty
   ];
 }
