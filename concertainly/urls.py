3489518585dde7b37737564dc7f9e26aff733c4a
Marcus Looi <2999673l@student.gla.ac.uk>
2026-03-06 15:05:24 +0000

    Added initial urls and views

--- concertainly/urls.py
+++ concertainly/urls.py
@@ -1,8 +1,17 @@
 from django.urls import path
-
 from concertainly import views
 
 app_name = "concertainly"
+
 urlpatterns = [
     path("", views.index, name="index"),
+    path("home/", views.home, name="home"),
+    path("search/", views.search, name="search"),
+    path("register/", views.register, name="register"),
+    path("login/", views.login, name="login"),
+    path("me/", views.account, name="account"),
+    path("genres/", views.genre_list, name="genre"),
+    path("genres/<str:genre_name>/", views.genre_detail, name="genre_detail"),
+    path("artist/<str:artist_name>/", views.artist_detail, name="artist_detail"),
+    path("tour/<str:tour_name>/", views.tour_detail, name="tour_detail"),
 ]
--- concertainly/views.py
+++ concertainly/views.py
@@ -1,10 +1,35 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from concertainly.models import *
 
 
 def index(request):
     context_dict = {"boldmessage": "Crunchy, creamy, cookie, candy, cupcake!"}
     return render(request, "index.html", context=context_dict)
 
+def home(request):
+    return render(request, "home.html")
 
-# Create your views here.
+def search(request):
+    return render(request, "search.html")
+
+def register(request):
+    return render(request, "register.html")
+
+def login(request):
+    return render(request, "login.html")
+
+def account(request):
+    return render(request, "account.html")
+
+def genre_list(request):
+    return render(request, "genre_list.html")
+
+def genre_detail(request, genre_name):
+    return render(request, "genre_detail.html", {"genre_name": genre_name})
+
+def artist_detail(request, artist_name):
+    return render(request, "artist_detail.html", {"artist_name": artist_name})
+
+def tour_detail(request, tour_name):
+    return render(request, "tour_detail.html", {"tour_name": tour_name})
