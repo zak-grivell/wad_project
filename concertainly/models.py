82e3ae03dea03aec0b9f8dedae85d9351872b6e7
smhmyhead <fares.sawalhi@gmail.com>
2026-03-06 15:02:45 +0000

    updated ConcertUser

--- concertainly/models.py
+++ concertainly/models.py
@@ -2,6 +2,7 @@ import uuid
 from django.db.models.deletion import CASCADE
 from django.core.validators import MinValueValidator, MaxValueValidator
 from django.db import models
+from django.contrib.auth.models import User
 
 
 class Artist(models.Model):
@@ -9,10 +10,13 @@ class Artist(models.Model):
     name = models.CharField(max_length=128)
 
 
-class User(models.Model):
+class ConcertUser(models.Model):
+    # associate a ConcertUser to each user
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
     id = models.UUIDField(default=uuid.uuid4, editable=False, unique=True, primary_key = True)
-    name = models.CharField(max_length=128)
-    password = models.CharField(max_length=64)
+    
+    def __str__(self):
+        return self.user.username
 
 class Tour(models.Model):
     id = models.UUIDField(default=uuid.uuid4, editable=False, unique=True, primary_key = True)
