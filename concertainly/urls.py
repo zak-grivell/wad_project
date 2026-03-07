e6681143d1714365c3ed15ceb11258d51023a141
zak grivell <zak@grivell.uk>
2026-03-07 00:39:14 +0000

    add spotify api

--- concertainly/urls.py
+++ concertainly/urls.py
@@ -5,4 +5,5 @@ from concertainly import views
 app_name = "concertainly"
 urlpatterns = [
     path("", views.index, name="index"),
+    path("spotify/", views.spotify_test, name="spotify"),
 ]
--- concertainly/views.py
+++ concertainly/views.py
@@ -1,9 +1,15 @@
 from django.http import HttpResponse
 from django.shortcuts import render
-
+from services.spotify import SpotifyAPI
 
 def index(request):
     context_dict = {"boldmessage": "Crunchy, creamy, cookie, candy, cupcake!"}
+    return render(request, "index.html", context=context_dict)
+
+def spotify_test(request):
+    s = SpotifyAPI()
+    
+    context_dict = {"boldmessage": str(s.artist("06HL4z0CvFAxyc27GXpf02"))}
     return render(request, "index.html", context=context_dict)
 
 
