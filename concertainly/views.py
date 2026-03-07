911f472fbb1e0a64e306335a5a5777ef27ae4ca5
Marcus Looi <2999673l@student.gla.ac.uk>
2026-03-07 16:16:30 +0000

    add user registration and login functionality

--- concertainly/views.py
+++ concertainly/views.py
@@ -1,6 +1,7 @@
 from django.http import HttpResponse
 from django.shortcuts import render, get_object_or_404
 from concertainly.models import Genre, Artist, Tour
+from concertainly.forms import UserForm, UserProfileForm
 from django.shortcuts import redirect 
 from django.urls import reverse
 from django.contrib.auth import authenticate, login, logout
@@ -23,10 +24,56 @@ def search(request):
     return render(request, "search.html")
 
 def user_register(request):
-    return render(request, "register.html")
+    registered = False
+
+    if request.method =='POST':
+        user_form = UserForm(request.POST)
+        profile_form = UserProfileForm(request.POST)
+
+        if user_form.is_valid() and profile_form.is_valid():
+            user = user_form.save()
+
+            user.set_password(user.password)
+            user.save()
+
+            profile = profile_form.save(commit=False)
+            profile.user = user
+            
+            profile.save()
+
+            registered = True
+
+            return redirect("concertainly:user_login")
+        else:
+            print(user_form.errors, profile_form.errors)
+    else:
+        user_form = UserForm()
+        profile_form = UserProfileForm()
+    
+    return render(request,
+                  "register.html",
+                  context = {"user_form": user_form,
+                             "profile_form": profile_form,
+                             "registered": registered})
 
 def user_login(request):
-    return render(request, "login.html")
+    if request.method == "POST":
+        username = request.POST.get("username")
+        password = request.POST.get("password")
+
+        user = authenticate(username=username, password=password)
+
+        if user:
+            if user.is_active:
+                login(request, user)
+                return redirect(reverse("concertainly:home"))
+            else:
+                return HttpResponse("Your Concertainly account is disabled.")
+        else:
+            print(f"Invalid login details")
+            return HttpResponse("Invalid login details supplied.")
+    else:
+        return render(request, "login.html")
 
 @login_required
 def user_logout(request):
