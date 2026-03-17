ac2ada9dde14fbffc68bf77d0f5cc861f51e07bd
smhmyhead <fares.sawalhi@gmail.com>
2026-03-17 11:16:00 +0000

    updates to register page

--- concertainly/views.py
+++ concertainly/views.py
@@ -48,28 +48,29 @@ def search(request):
 
 def user_register(request):
     registered = False
-
-    if request.method =='POST':
+    
+    # if it's a post request, process the data
+    if request.method == "POST":
+        # grab form data
         user_form = UserForm(request.POST)
 
+        # NOTE: if we ever add more info on each user (necessitating the existence of ConcertUser or something) we'll need a new form and a check that it's valid
         if user_form.is_valid():
+            # save data
             user = user_form.save()
 
+            # set_password does password hashing
             user.set_password(user.password)
             user.save()
 
             registered = True
-
-            return redirect("concertainly:user_login")
         else:
+            # if invalid, complain to the terminal
             print(user_form.errors)
     else:
         user_form = UserForm()
-    
-    return render(request,
-                  "register.html",
-                  context = {"user_form": user_form,
-                             "registered": registered})
+
+    return render(request, 'register.html', context = {'user_form': user_form, 'registered': registered})
 
 def spotify_test(request):
     s = SpotifyAPI()
