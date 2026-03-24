35417efcb52c6e1e731c8b49e4dd79441e5fe65a
smhmyhead <fares.sawalhi@gmail.com>
2026-03-24 10:42:11 +0000

    auth works!

--- concertainly/views.py
+++ concertainly/views.py
@@ -52,24 +52,32 @@ def user_register(request):
     print("in register")
     # if it's a post request, process the data
     if request.method == "POST":
+        print("form received!")
         # grab form data
+        print("kuasybdf")
         user_form = UserForm(request.POST)
-
+        print("form saved")
+        print(user_form.is_valid())
         # NOTE: if we ever add more info on each user (necessitating the existence of ConcertUser or something) we'll need a new form and a check that it's valid
         if user_form.is_valid():
+            print("form was valid")
             # save data
             user = user_form.save()
-
+            print("1")
             # set_password does password hashing
             user.set_password(user.password)
+            print("2")
             user.save()
-
+            print("3")
             registered = True
             print("registered successfully")
         else:
+            print("form was invalid")
             # if invalid, complain to the terminal
             print(user_form.errors)
+            
     else:
+        print("not post")
         user_form = UserForm()
 
     return render(request, 'register.html', context = {'user_form': user_form, 'registered': registered})
